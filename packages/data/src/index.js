d735c2717c947ff2599151a19e22c4dea9ca5e32
Jeff Stieler <jeff.m.stieler@gmail.com>
2020-05-08 13:35:06 -0600

    Export user prefs hook and HoC in data package.

--- packages/data/src/index.js
+++ packages/data/src/index.js
@@ -4,3 +4,6 @@ export { useSettings } from './settings/use-settings';
 
 export { PLUGINS_STORE_NAME } from './plugins';
 export { withPluginsHydration } from './plugins/with-plugins-hydration';
+
+export { withCurrentUserHydration } from './user-preferences/with-current-user-hydration';
+export { useUserPreferences } from './user-preferences/use-user-preferences';
